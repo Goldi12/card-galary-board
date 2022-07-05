e5430e424dba801948c34907c36f4981f89f8085
Vijay Kumar Gupta <guptavinay225@gmail.com>
2022-07-05 08:18:01 +0530

    first commit

---
+++ porfolio-galary/js/app.js
@@ -0,0 +1,16 @@
+
+const filter_btns = document.querySelectorAll(".filter-btn");
+filter_btns.forEach( btn => btn.addEventListener("click", ()=> {
+  filter_btns.forEach(button => button.classList.remove("active"));
+  btn.classList.add("active");
+  let filterValue = btn.dataset.filter;
+  $(".grid").isotope({filter: filterValue});
+
+})
+);
+  
+$('.grid').isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows',
+    TransitionDuration: "0.6s", 
+  });
